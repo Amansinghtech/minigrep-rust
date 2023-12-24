df91f38319687975036b66f134862a8f71a759a2
Aman Singh <singhaman11415@gmail.com>
2023-12-25 04:31:31 +0530

    added basics to take cli arguments and read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filepath = &args[2];
+
+    println!("Searching for: \"{query}\"");
+    println!("In File: {filepath}");
+
+    let content = fs::read_to_string(filepath)
+        .expect("Should have been able to read the file");
+
+    println!("file content: \n{content}")
 }
